33fecc802ff8e8912f5817c5ddaec3b24c92b695
Sam Ashtar <samashtarm@gmail.com>
2018-06-29 04:20:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,4 @@
 var playlist = {Prime: "I've Changed"}
-updatePlaylist(playlist, artistName, songTitle)
+function updatePlaylist(playlist, artistName, songTitle) {
+   playlist.artistName = songTitle;
+}
