75d40b954a6739188963636202339b9ce5b7d3e2
Sam Ashtar <samashtarm@gmail.com>
2018-06-29 04:31:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist: "songtitle"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['artistName'] = 'songTitle';
+  playlist[artistName] = 'songTitle';
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
